5d4793416f59689a5329a66192d1ba926a7bc124
CharleneHoo <CharleneHoo@hotmail.com>
2026-09-21 16:51:23 +0800

    7

--- lua/cda/playback/animation_source.lua
+++ lua/cda/playback/animation_source.lua
@@ -14,18 +14,18 @@ end
 local function acquireEntity(modelName)
     if type(modelName) ~= "string" or not util.IsValidModel(modelName) then return nil end
 
-    local entity = ents.Create("prop_dynamic")
-    if not IsValid(entity) then return nil end
+    local ent = ents.Create("prop_dynamic")
+    if not IsValid(ent) then return nil end
 
-    entity:SetModel(modelName)
-    entity:Spawn()
-    return entity
+    ent:SetModel(modelName)
+    ent:Spawn()
+    return ent
 end
 
----@param entity Entity
-local function releaseEntity(entity)
-    if IsValid(entity) then
-        entity:Remove()
+---@param ent Entity
+local function releaseEntity(ent)
+    if IsValid(ent) then
+        ent:Remove()
     end
 end
 
@@ -34,7 +34,9 @@ local Thinker = include("cda/core/thinker.lua")
 ---@class AnimationSource:Thinker
 ---@field Ent Entity
 ---@field SequenceID number
----@field Duration number
+---@field AnchorID number
+---@field ShouldLoop boolean
+---@field EndTime number
 ---@field Play fun(self: AnimationSource)
 
 ---@class AnimationSourceClass:ThinkerClass
@@ -59,11 +61,39 @@ function AnimationSource:New(track)
         return nil
     end
 
+    local anchorID = getAnchorID(ent)
+    if not anchorID or type(anchorID) ~= "number" or anchorID < 0 then
+        instance:Remove()
+        return nil
+    end
+
+    local duration = track.Duration or sequenceDuration or math.huge
+    local endTime = CurTime() + duration
+
     instance.Ent = ent
     instance.SequenceID = sequenceID
-    instance.Duration = track.Duration or sequenceDuration or math.huge
+    instance.AnchorID = anchorID
+    instance.ShouldLoop = track.CanLoop
+    instance.EndTime = endTime
 
     return instance
+end
+
+local PELVIS = "ValveBiped.Bip01_Pelvis"
+
+---@param ent AnimationSource
+---@return number|nil id
+local function getAnchorID(ent)
+    local ent = instance.Ent
+    return ent:LookupBone(PELVIS)
+end
+
+---@param self AnimationSource
+---@return Vector 
+function AnimationSource:GetAnchorPos()
+    if not self.AnchorID then
+        self.AnchorID = ent:LookupBone(PELVIS)
+    end
 end
 
 ---@param self AnimationSource
@@ -71,6 +101,21 @@ function AnimationSource:Play()
     self.Ent:ResetSequence(self.SequenceID)
     self.Ent:ResetSequenceInfo()
     self.Ent:SetCycle(0)
+
+    timer.Simple(0, function ()
+        local startPos = self.Ent:
+    end)
+end
+
+---@param self AnimationSource
+function AnimationSource:_Think()
+    local now = CurTime()
+    if now < self.EndTime then return end
+
+    if self.ShouldLoop then
+    else
+        self:Remove()
+    end
 end
 
 ---@param self AnimationSource
--- lua/cda/types/playback.lua
+++ lua/cda/types/playback.lua
@@ -3,6 +3,7 @@
 ---@class Track
 ---@field ModelName string @动画模型路径
 ---@field SequenceName string @动画序列名
+---@field CanLoop boolean @是否支持循环播放
 ---@field Duration number? @播放时长（秒）
 
 ---@alias Scene Track[]
