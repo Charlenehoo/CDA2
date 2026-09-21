311a2bc0f848fb151cde1d15d2769de447a1c465
CharleneHoo <CharleneHoo@hotmail.com>
2026-09-21 09:43:52 +0800

    feat(core): 添加 helper 库与 SwapRemove

---
+++ lua/cda/core/helper.lua
@@ -0,0 +1,26 @@
+-- lua/cda/core/helper.lua
+
+local helper = {}
+
+---从密集数组中 O(1) 移除指定下标的元素
+---若被移除的不是末尾元素，则末尾元素会填补空位并作为第二个返回值返回
+---@param array table
+---@param index number
+---@return any removed
+---@return any moved
+function helper.SwapRemove(array, index)
+    local lastIndex = #array
+    local removed = array[index]
+
+    if index ~= lastIndex then
+        local moved = array[lastIndex]
+        array[index] = moved
+        array[lastIndex] = nil
+        return removed, moved
+    end
+
+    array[lastIndex] = nil
+    return removed, nil
+end
+
+return helper
