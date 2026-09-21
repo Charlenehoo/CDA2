ac3eaaa15ba9ecd201cfbf0a10d6484981ab9ed4
CharleneHoo <CharleneHoo@hotmail.com>
2026-09-21 14:38:19 +0800

    2

--- lua/cda/adapter/bsmod_adapter.lua
+++ lua/cda/adapter/bsmod_adapter.lua
@@ -14,75 +14,61 @@ local BSMOD_EVENT_RequestCustomKillMove = "CustomKillMoves"
 
 local Adepter = {}
 
+---@class BSModCustomKillMoveResult
+---@field [1] string? PlyKMModel 玩家 killmove 模型路径
+---@field [2] string? TargetKMModel 目标 killmove 模型路径
+---@field [3] string? AnimName 动画序列名
+---@field [4] Vector? PlyKMPosition 玩家落点
+---@field [5] Angle? PlyKMAngle 玩家朝向
+---@field [6] number? PlyKMTime 玩家动画时长
+---@field [7] number? TargetKMTime 目标动画时长
+---@field [8] boolean? MoveTarget 是否交换玩家和目标
+
 ---@param bsResult BSModCustomKillMoveResult
----@return SceneDescriptor|nil
-local function parseSceneDescriptor(bsResult)
-    -- ① 必需的：victim 决定这是不是一个可解析的请求
+---@return Scene|nil
+local function parseSceneDescriptor(bsResult, killer, victim)
+    local sequenceName = bsResult[3]
+    if not sequenceName then return nil end
+
     local victimModel = bsResult[2]
     if not victimModel then return nil end
 
-    -- ② 必需的：animName 决定能不能播
-    local animName = bsResult[3]
-    if not animName then return nil end
-
-    -- ③ 可选：killer 由 model 是否存在决定
     local killerModel = bsResult[1]
-    local killer
-    if killerModel then
-        killer = {
-            Model    = killerModel,
-            Duration = bsResult[6], -- 可为 nil，由消费方兜底
-        }
-    end
+    if not killerModel then return nil end
 
-    ---@type SceneDescriptor
+    ---@type Track
+    local victimTrack = {
+        ModelName = victimModel,
+        SequenceName = sequenceName,
+        Position = Vector(0, 0, 0),
+        Angle = Angle(0, 0, 0),
+        Duration = bsResult[7],
+    }
+
+    ---@type Track
+    local killerTrack = {
+        ModelName = victimModel,
+        SequenceName = sequenceName,
+        Position = Vector(0, 0, 0),
+        Angle = Angle(0, 0, 0),
+        Duration = bsResult[7],
+    }
+
+    ---@type Scene
     return {
-        AnimName          = animName,
-        Victim            = {
-            Model    = victimModel,
-            Duration = bsResult[7], -- 可为 nil
-        },
-        Killer            = killer,
-        ShouldAlignVictim = bsResult[8],
+        victimTrack,
+        killerTrack,
     }
 end
 
 local EVENT = Event.RequestCustomKillMove
 local ID = KEY .. "_" .. EVENT
-hook.Add(EVENT, ID, function (ply, target, angleAround)
+hook.Add(EVENT, ID, function (killer, victim, angleAround)
     ---@type BSModCustomKillMoveResult?
-    local bsModCustomKillMoveResult = hook.Run("CustomKillMoves", ply, target, angleAround)
+    local bsModCustomKillMoveResult = hook.Run("CustomKillMoves", killer, victim, angleAround)
     if not bsModCustomKillMoveResult then return nil end
 
-    local animName = bsModCustomKillMoveResult[3]
-    if not animName then return nil end
-
-    local victimModel = bsModCustomKillMoveResult[2]
-    local victimDuration = bsModCustomKillMoveResult[7]
-    if not victimModel or not victimDuration then return nil end
-    local victim = {
-        Model = victimModel,
-        Duration = victimDuration
-    }
-
-    local killerModel = bsModCustomKillMoveResult[1]
-    local killerDuration = bsModCustomKillMoveResult[6]
-    local killer
-    if killerModel and killerDuration then
-        killer = {
-            Model = killerModel,
-            Duration = killerDuration
-        }
-    end
-
-    ---@type SceneDescriptor
-    local myCustomKillMoveResult = {
-        AnimName = animName,
-        Victim = victim,
-        Killer = killer
-    }
-
-    return myCustomKillMoveResult
+    return parseSceneDescriptor(bsModCustomKillMoveResult, killer, victim)
 end)
 
 package.loaded[KEY] = Adepter
