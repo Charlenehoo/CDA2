7cdef24a399774c3eee6b8c4e4a90ab08d5ce384
CharleneHoo <CharleneHoo@hotmail.com>
2026-09-21 10:02:43 +0800

    style(core): helper.SwapRemove 改类型注解, 明确是密集表

--- lua/cda/core/helper.lua
+++ lua/cda/core/helper.lua
@@ -4,7 +4,7 @@ local helper = {}
 
 ---从密集数组中 O(1) 移除指定下标的元素
 ---若被移除的不是末尾元素，则末尾元素会填补空位并作为第二个返回值返回
----@param array table
+---@param array any[]
 ---@param index number
 ---@return any removed
 ---@return any moved
