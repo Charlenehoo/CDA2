c83202fe237ec8d222a6bf01053c491977f690b1
CharleneHoo <CharleneHoo@hotmail.com>
2026-09-22 08:53:38 +0800

    14

--- lua/cda/core/log/init.lua
+++ lua/cda/core/log/init.lua
@@ -1,11 +1,14 @@
+local Constants = include("cda/core/constants.lua")
+
+local ADDON_NAME = Constants.ADDON_NAME
 local MODULE_NAME = "Log"
-_EDAE2Singletons = _EDAE2Singletons or {}
-if _EDAE2Singletons[MODULE_NAME] then
-    return _EDAE2Singletons[MODULE_NAME]
+local KEY = ADDON_NAME .. "_" .. MODULE_NAME
+if package.loaded[KEY] then
+    return package.loaded[KEY]
 end
 
 local log = include("edae2/core/log/log.lua")
 log.level = "trace"
 
-_EDAE2Singletons[MODULE_NAME] = log
+package.loaded[KEY] = log
 return log
